ff7e778f8de69d14665d574395b16fb35e07b332
mac <mac@macs-MacBook-Pro.local>
2018-04-26 02:59:30 -0400

    footer and header

---
+++ assets/php/contact.php
@@ -0,0 +1,34 @@
+<?php
+
+if(isset($_POST['message'])){
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+    
+	
+	$to      = 'contact@ramonterry.com';
+	$subject = 'Site Contact Form';
+
+	$headers = 'From: '. $email . "\r\n" .
+    'Reply-To: '. $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+	$status = mail($to, $subject, $message, $headers);
+
+	if($status == TRUE){	
+		$res['sendstatus'] = 'done';
+	
+		//Edit your message here
+		$res['message'] = 'Form Submission Successful';
+    }
+	else{
+		$res['message'] = 'Failed to send mail. Please mail me to contact@ramonterry.com';
+	}
+	
+	
+	echo json_encode($res);
+}
+
+?>
+
