8ab28063d0b1edb7de7b47ec699ced509a9631d8
mac <mac@macs-MacBook-Pro.local>
2018-05-09 11:36:55 -0400

    meta tags link sharing

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,14 +23,14 @@ function multiplyBy()
     return false;
   }
 
-  function checkEmail() {
+ //  function checkEmail() {
 
-    var email = document.getElementById('email');
-    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+ //    var email = document.getElementById('email');
+ //    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-    if (!filter.test(email.value)) {
-    alert('Please provide a valid email address');
-    email.focus;
-    return false;
- }
+ //    if (!filter.test(email.value)) {
+ //    alert('Please provide a valid email address');
+ //    email.focus;
+ //    return false;
+ // }
 }
